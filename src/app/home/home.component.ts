21d21c5e96490050d7806247933ad7cc1d67b96b
seyyed hassan moosavi nazar <73283752+moosavinazar@users.noreply.github.com>
2021-12-15 18:38:11 +0330

    create first observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable, Observer} from "rxjs";
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,18 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    const interval$ = new Observable((observer: Observer<number>) => {
+      observer.next(1);
+      observer.next(2);
+      observer.next(3);
+      observer.error(new Error('Error in app!'))
+    });
+
+    interval$.subscribe(
+      (val) => { console.log(val); }
+    )
+
   }
 
 }
