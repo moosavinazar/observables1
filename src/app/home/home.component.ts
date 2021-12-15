77cc46b03fa4c41d7a6d01d6fe0208ca539c821d
seyyed hassan moosavi nazar <73283752+moosavinazar@users.noreply.github.com>
2021-12-15 18:55:20 +0330

    use unsubscribe

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import {Observable, Observer} from "rxjs";
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Observable, Observer, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  intervalSubscription!: Subscription;
 
   constructor() { }
 
   ngOnInit(): void {
+
+    /*document.addEventListener('click', (event) => {
+      console.log(event);
+    });*/
 
     const interval$ = new Observable((observer: Observer<number>) => {
       let count = 0;
@@ -18,17 +24,21 @@ export class HomeComponent implements OnInit {
         observer.next(count++);
         if (count ===5) {
           //observer.error(new Error('We reached 5'));
-          observer.complete();
+          //observer.complete();
         }
       },1000)
     });
 
-    interval$.subscribe(
+    this.intervalSubscription = interval$.subscribe(
       (val) => { console.log(val); },
       (error) => { console.log(error); },
       () => { console.log('Completed'); }
-    )
+    );
 
+  }
+
+  ngOnDestroy(): void {
+    this.intervalSubscription.unsubscribe();
   }
 
 }
